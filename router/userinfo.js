b0830506cdf054db19fbdc828ca16271ddf7d25c
zz <2366109794@qq.com>
2024-03-27 19:12:49 +0800

    管理员权限-用户列表

--- router/userinfo.js
+++ router/userinfo.js
@@ -11,19 +11,31 @@ const expressJoi = require('@escook/express-joi')
 // 导入需要的验证规则对象
 const { update_userinfo_schema, update_password_schema, update_avatar_schema } = require('../schema/user')
 
+// 当前用户端
 // 获取用户基本信息的路由
 router.get('/userinfo', userinfo_handler.getUserInfo)
+
+// 教师端
 // 获取学生列表的路由
 router.get('/studentlist', userinfo_handler.getStudentList)
 // 删除学生的路由
 router.delete('/deleteuser/:id', userinfo_handler.deleteUser);
 // 编辑学生的路由
 router.put('/edituser/:id', userinfo_handler.editUser);
-// 更新用户信息的路由
-router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
+
 // 更新密码的路由
 router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 // 更换头像的路由
 router.post('/update/avatar', expressJoi(update_avatar_schema), userinfo_handler.updateAvatar)
+
+// admin端
+// 获取学生和教师列表的路由
+router.get('/admingetuserlist', userinfo_handler.adminGetUserlist)
+// 删除用户的路由
+router.delete('/admindeleteuser/:id', userinfo_handler.adminDeleteUser)
+// 编辑用户的路由
+router.put('/adminedituser/:id', userinfo_handler.adminEditUser)
+
+
 
 module.exports = router
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -3,6 +3,7 @@ const db = require('../db/index')
 // 导入处理密码的模块
 const bcrypt = require('bcryptjs')
 
+// 当前用户端
 // 获取当前用户基本信息的处理函数
 exports.getUserInfo = (req, res) => {
   // 定义查询用户信息的 SQL 语句
@@ -22,6 +23,8 @@ exports.getUserInfo = (req, res) => {
     })
   })
 }
+
+// 教师端
 // 获取所有学生列表的处理函数
 exports.getStudentList = (req, res) => {
   const sql = `SELECT id, student_id, nickname, classname, major FROM users WHERE role='student'`
@@ -87,20 +90,6 @@ exports.editUser = (req, res) => {
     });
   });
 };
-// 更新用户基本信息的处理函数
-exports.updateUserInfo = (req, res) => {
-  // 定义待执行的 SQL 语句
-  const sql = `update users set ? where id=?`
-  // 调用 db.query() 执行 SQL 语句并传递参数
-  db.query(sql, [req.body, req.body.id], (err, results) => {
-    // 执行 SQL 语句失败
-    if (err) return res.cc(err)
-    // 执行 SQL 语句成功，但是影响行数不等于 1
-    if (results.affectedRows !== 1) return res.cc('更新用户的基本信息失败！')
-    // 成功
-    res.cc('更新用户信息成功！', 0)
-  })
-}
 
 // 更新用户密码的处理函数
 exports.updatePassword = (req, res) => {
@@ -132,7 +121,6 @@ exports.updatePassword = (req, res) => {
     })
   })
 }
-
 // 更新用户头像的处理函数
 exports.updateAvatar = (req, res) => {
   // 1. 定义更新头像的 SQL 语句
@@ -146,4 +134,71 @@ exports.updateAvatar = (req, res) => {
     // 成功
     res.cc('更换头像成功！', 0)
   })
+}
+
+// admin端
+// 获取全部学生和教师的基本信息的处理函数
+exports.adminGetUserlist = (req, res) =>{
+  const sql = `SELECT id, username, nickname, student_id, classname, major, role FROM users where role='student' OR role='teacher'`
+  db.query(sql, (err, results) => {
+    if (err) return res.cc(err)
+    res.send({
+      status: 0,
+      message: '获取用户列表成功！',
+      data: results,
+    })
+  })
+}
+// 删除用户的处理函数
+exports.adminDeleteUser = (req, res) =>{
+  const userId = req.params.id; // 获取前端传来的用户 ID
+
+  const sql = 'DELETE FROM users WHERE id = ?'; 
+
+  db.query(sql, [userId], (err, results) => { 
+    if (err) { // 错误处理
+      return res.status(500).json({
+        status: 500,
+        message: '删除用户失败',
+        error: err.message
+      });
+    }
+    // 如果未找到符合条件的记录
+    if (results.affectedRows === 0) { 
+      return res.status(404).json({
+        status: 404,
+        message: '未找到符合条件的用户信息'
+      });
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: '删除用户成功',
+      data: results
+    });
+  });
+}
+// 编辑用户的处理函数
+exports.adminEditUser = (req, res) =>{
+  const userId = req.params.id;
+  const { username, nickname, student_id, classname, major, role } = req.body;
+  // 构造 SQL 更新语句
+  const sql = `UPDATE users SET student_id=?, username=?, nickname=?, classname=?, major=?,role=? WHERE id=?`;
+  // 执行 SQL 更新操作
+  db.query(sql, [student_id, username, nickname, classname, major, role, userId], (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        status: 500,
+        message: '保存编辑失败',
+        error: err.message
+      });
+    }
+
+    // 返回保存成功的响应
+    res.status(200).json({
+      status: 200,
+      message: '保存编辑成功',
+      data: results
+    });
+  });
 }
